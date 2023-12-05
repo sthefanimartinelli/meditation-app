62d5656b43cf2f31c0e260af2e99e64b650dbb38
Sthéfani <sthefani_martinelli@hotmail.com>
2023-12-05 20:07:30 -0300

    feat: add search endpoint

---
+++ src/components/SearchButton.tsx
@@ -0,0 +1,9 @@
+function SearchButton() {
+  return (
+    <button className="btn btn-primary" type="submit">
+      Search
+    </button>
+  );
+}
+
+export default SearchButton;
---
+++ src/pages/Search.tsx
@@ -0,0 +1,14 @@
+import SearchBar from '../components/SearchBar';
+import SearchButton from '../components/SearchButton';
+
+function Search() {
+  return (
+    <div>
+      <h1>Search</h1>
+      <SearchBar />
+      <SearchButton />
+    </div>
+  );
+}
+
+export default Search;
